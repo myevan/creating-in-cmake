b99d8c2b8132ca94721fe7919aac334780f622ad
Song Young-jin <myevan80@gmail.com>
2015-02-26 02:00:57 +0900

    + 21_library

---
+++ Examples/21_library/custom_funcs.cpp
@@ -0,0 +1,6 @@
+#include "custom_funcs.h"
+
+float dot(float v1x, float v1y, float v2x, float v2y)
+{
+    return v1x * v2x + v1y * v2y;
+}
---
+++ Examples/21_library/custom_funcs.h
@@ -0,0 +1,6 @@
+#ifndef __CUSTOM_FUNCS__
+#define __CUSTOM_FUNCS__
+
+float dot(float v1x, float v1y, float v2x, float v2y);
+
+#endif
